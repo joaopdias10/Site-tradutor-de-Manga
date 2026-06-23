6be1729ef287ae86a3214669930fe78f608b210f
João Pedro <joaopedromeurerdias86@gmail.com>
2026-06-23 20:43:06 -0300

    Renomeia translator.py para manga_translator.py

---
+++ Backend/manga_translator.py
@@ -0,0 +1,78 @@
+from ultralytics import YOLO
+import cv2
+import pytesseract
+import re
+import os
+import shutil
+
+from deep_translator import GoogleTranslator
+from PIL import Image, ImageDraw
+
+from .translator.translator import translate
+from .lettering.lettering import spell
+
+# ============================================================
+# Tesseract (Windows + Linux / Render)
+# ============================================================
+tesseract_cmd = shutil.which("tesseract")
+
+if tesseract_cmd:
+    pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
+else:
+    raise RuntimeError("Tesseract não encontrado no sistema")
+
+
+# ============================================================
+# Caminhos seguros
+# ============================================================
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "weight", "best.pt")
+font_path = os.path.join(BASE_DIR, "font", "KOMIKAX_.ttf")
+
+# ============================================================
+# Carrega modelo UMA VEZ
+# ============================================================
+model = YOLO(MODEL_PATH)
+
+
+# ============================================================
+# Função principal
+# ============================================================
+def traduz_manga(input_image_path: str, output_image_path: str):
+    # Lê imagem
+    image = cv2.imread(input_image_path)
+    if image is None:
+        raise ValueError("Imagem não encontrada")
+
+    # YOLO
+    results = model(image)
+
+    # OpenCV → PIL
+    image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(image_pil)
+
+
+    for box in results[0].boxes:
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+        cropped = image[y1:y2, x1:x2]
+
+        config = r"--oem 3 --psm 6"
+        text = pytesseract.image_to_string(cropped, config=config)
+
+        text = re.sub(r"\s+", " ", text).strip()
+        if not text:
+            continue
+
+        text = text.capitalize()
+        text = GoogleTranslator(source="en", target="pt").translate(text)
+        #text = translate(text)
+
+        # Pinta o balão de branco
+        draw.rectangle([x1, y1, x2, y2], fill=(255, 255, 255))
+
+
+        spell(draw, text, x1, y1, x2, y2, font_path)
+
+    # Salva resultado
+    image_pil.save(output_image_path)
